6a9e220e2c989a043ecbd7b32d4749d71ce10998
fromjyce <jaya2004kra@gmail.com>
2025-03-04 10:25:21 +0530

    changed the fonts and bg

--- frontend/src/components/StoryPage.js
+++ frontend/src/components/StoryPage.js
@@ -18,21 +18,21 @@ function StoryPage() {
 
 
         <h1 className='welcome-message pt-serif-bold'>Journey to Sphoorthy's Vault!</h1>
-        <p className="story-text pt-serif-regular">
+        <p className="story-text">
           Long ago, a wise mentor named Sphoorthy foresaw a time when true wisdom would be needed to guide the world. 
           To protect her greatest knowledge, she locked it away in a magical treasure vault in the legendary Land of Enlightenment.
         </p>
-        <p className="story-text pt-serif-regular">
+        <p className="story-text">
           She filled the land with puzzles and challenges to ensure only the most determined and clever seekers could unlock the vault. 
           Now, as a treasure hunter, your mission is to explore the land, solve puzzles, and complete tasks to prove your worth.
         </p>
-        <p className="story-text pt-serif-regular">
+        <p className="story-text">
           Unlocking the vault will grant you Sphoorthy's ultimate treasure: her timeless wisdom, which holds the power to enlighten and guide those who seek it.
         </p>
-        <p className="glowing-text pt-serif-regular-italic">
+        <p className="glowing-text">
           Sphoorthy's spirit watches over you, offering cryptic hints and special tasks when needed.
         </p>
-        <button className="go-button pt-serif-regular-italic" onClick={goToLevelsPage}>
+        <button className="go-button" onClick={goToLevelsPage}>
           Begin Your Quest
         </button>
 

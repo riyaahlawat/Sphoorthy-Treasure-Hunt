76049a49ad723653f4f9f9874bcfb03a9c46b4c6
fromjyce <jaya2004kra@gmail.com>
2025-03-14 11:42:53 +0530

    added button sound

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,26 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import Sound from "react-sound";
 import "./Login.css";
+import buttonClickSound from "../assets/sound-effects/button-click.mp3";
 
 const LandingPage = () => {
   const navigate = useNavigate();
+  const [playButtonSound, setPlayButtonSound] = useState(false);
 
   const handleStart = () => {
-    navigate("/story-page"); // Navigate to the start page
+    setPlayButtonSound(true);
+    setTimeout(() => {
+      navigate("/story-page");
+    }, 500);
   };
 
   return (
     <div className="landing-page">
-      <div className="overlay"></div> {/* Dark overlay for better text visibility */}
+      <div className="overlay"></div>
       <div className="content">
         <h1 className="welcome-message">Welcome to Treasure Hunt!</h1>
         <p className="description">
           Embark on an exciting adventure to uncover hidden treasures and solve challenging puzzles. Are you ready to prove your skills and claim the ultimate prize?
         </p>
         <button className="start-button" onClick={handleStart}>
-        Claim the Treasure!
+          Claim the Treasure!
         </button>
       </div>
+      <Sound
+        url={buttonClickSound}
+        playStatus={playButtonSound ? Sound.status.PLAYING : Sound.status.STOPPED}
+        onFinishedPlaying={() => setPlayButtonSound(false)}
+        volume={100}
+      />
     </div>
   );
 };
